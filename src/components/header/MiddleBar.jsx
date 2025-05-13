56f6d93246b72b9db6c3c82520fcc02bf2a185b5
Vercel <vercel@devsramen.com>
2025-05-13 17:13:34 +0600

    Update  MiddleBar.jsx file at 13-05-2025_1713.

--- src/components/header/MiddleBar.jsx
+++ src/components/header/MiddleBar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Container from '../commonLayouts/Container';
 import { IoSearchSharp } from "react-icons/io5";
 import CartIcon from '../../assets/icons/CartIcon';
+import UserIcon from '../../assets/icons/UserIcon';
 
 const MiddleBar = () => {
     return (
@@ -12,17 +13,27 @@ const MiddleBar = () => {
                         <img src="images/logo.svg" alt="logo" />
                     </div>
                     <div className='flex justify-end items-center'>
-                        <div className='relative mr-12'>
+                        <div className='relative mr-2'>
                             <input 
                             type="text"
                             className='w-[332px] border border-[#cccccc] py-[18px] px-6 rounded-[10px]'
                             placeholder='Search Products ...'/>
                             <IoSearchSharp className='w-4 text-3xl absolute top-1/2 right-6 -translate-y-1/2'/>
                         </div>
-                        <div>
-                            <CartIcon/>
+                        <div className='w-[150px] relative pl-[50px] mr-16'>
+                            <CartIcon width={35} height={35} color="#303030"/>
+                            <div className='absolute top-1/2 -translate-y-1/2 left-24'>
+                                <p className='font-["Montserrat"] font-normal text-base leading-6'>cart</p>
+                                <span className='font-["Montserrat"] font-bold text-base leading-5' >$150,000</span>
+                            </div>
                         </div>
-                        <div>accounts Section</div>
+                        <div className='w-[170px] relative pl-[50px] after:content-[""] after:w-[1px] after:h-8 after:bg-[#303030] after:absolute after:left-0 after:top-1/2 after:-translate-y-1/2'>
+                            <UserIcon width={28} height={28} color="#303030"/>
+                            <div className='absolute top-1/2 -translate-y-1/2 left-24'>
+                                <p className='font-["Montserrat"] font-normal text-base leading-6'>User</p>
+                                <span className='font-["Montserrat"] font-bold text-base leading-5' >Account</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </Container>
