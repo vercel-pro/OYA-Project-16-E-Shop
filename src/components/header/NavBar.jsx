955295b507f7ad4859aa092bcb2e029f83e7a0ec
Ramen Biswas <vercel@devsramen.com>
2025-05-15 23:07:25 +0600

    Update NavBar.jsx

--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Container from '../commonLayouts/Container';
 import { Link } from 'react-router-dom';
 import { FaBars } from 'react-icons/fa';
 import { IoHandLeft } from 'react-icons/io5';
+import { RiArrowDownSLine } from 'react-icons/ri';
 
 const NavBar = () => {
+
+    const [isDropdownOpen, setIsDropdownOpen] = useState(true);
+    const dropdownRef = useRef(null)
+
+    useEffect(()=>{
+        const handleClickOutSide = (event)=>{
+            console.log(dropdownRef.current.contains(event.target),event.target)
+            if( dropdownRef.current && !dropdownRef.current.contains(event.target)){
+                setIsDropdownOpen(false)
+            }
+        }
+        document.addEventListener('mousedown', handleClickOutSide)
+    },[])
+
+    const handleProductDropdown = ()=>{
+        setIsDropdownOpen(!isDropdownOpen);
+    }
+
+
     return (
         <>
             <div className="bg-[#FF624C] py-6 text-white">
@@ -16,8 +36,22 @@ const NavBar = () => {
                                     <FaBars />
                                     <Link to={"/product-list"}>All Categories</Link>
                                 </li>
-                                <li>
-                                    <Link to={"/product-list"}>Products</Link>
+                                <li ref={dropdownRef} className='relative'>
+                                    <button onClick={handleProductDropdown} className='cursor-pointer flex items-center gap-x-1'>Products <RiArrowDownSLine className={`text-2xl ${isDropdownOpen && "rotate-180"}`}/></button>
+                                    {
+                                        isDropdownOpen && (
+                                            <div className="w-48 bg-white absolute z-10 mt-2 rounded-md shadow-lg overflow-hidden">
+                                                <ul className='py-2 font-["Montserrat] font-normal text-base leading-6 text-[#303030]'>
+                                                    <li className='px-4 py-2 transition-all duration-200 hover:bg-gray-200 cursor-pointer'>Product-1</li>
+                                                    <li className='px-4 py-2 transition-all duration-200 hover:bg-gray-200 cursor-pointer'>Product-2</li>
+                                                    <li className='px-4 py-2 transition-all duration-200 hover:bg-gray-200 cursor-pointer'>Product-3</li>
+                                                    <li className='px-4 py-2 transition-all duration-200 hover:bg-gray-200 cursor-pointer'>Product-4</li>
+                                                    <li className='px-4 py-2 transition-all duration-200 hover:bg-gray-200 cursor-pointer'>Product-5</li>
+                                                    <li className='px-4 py-2 transition-all duration-200 hover:bg-gray-200 cursor-pointer'>Product-6</li>
+                                                </ul>
+                                            </div>
+                                        )
+                                    }
                                 </li>
                                 <li>
                                     <Link to={"/blogs"}>Blogs</Link>
