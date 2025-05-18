7b42836ee414af084c7b1cc1b5c9b8afde96a289
Vercel <vercel@devsramen.com>
2025-05-18 09:32:08 +0600

    Update FacilityComponent.jsx

--- src/components/FacilityComponent.jsx
+++ src/components/FacilityComponent.jsx
@@ -9,28 +9,28 @@ const FacilityComponent = () => {
         <>
             <Container>
                 <div className="flex justify-between items-center mb-20">
-                    <div className="flex gap-x-6">
+                    <div className="flex items-center gap-x-6">
                         <HeadPhoneIcon/>
                         <div>
                             <h6 className='font-["Montserrat"] font-bold text-base'>Responsive</h6>
                             <p className='font-["Montserrat"] font-normal text-base'>Customer service available 24/7</p>
                         </div>
                     </div>
-                    <div className="flex gap-x-6">
+                    <div className="flex items-center gap-x-6">
                         <VerifiedIcon/>
                         <div>
                             <h6 className='font-["Montserrat"] font-bold text-base'>Secure</h6>
                             <p className='font-["Montserrat"] font-normal text-base'>Certified marketplace since 2017</p>
                         </div>
                     </div>
-                    <div className="flex gap-x-6">
+                    <div className="flex items-center gap-x-6">
                         <DeliveryIcon/>
                         <div>
                             <h6 className='font-["Montserrat"] font-bold text-base'>Shipping</h6>
                             <p className='font-["Montserrat"] font-normal text-base'>Free, fast, and reliable worldwide</p>
                         </div>
                     </div>
-                    <div className="flex gap-x-6">
+                    <div className="flex items-center gap-x-6">
                         <img src="images/transIcon.svg" alt="" />
                         <div>
                             <h6 className='font-["Montserrat"] font-bold text-base'>Transparent</h6>
