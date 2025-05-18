a0515b9b0e2ba665d3fcadcafccd69c2804c5c35
ramen72 <vercel@devsramen.com>
2025-05-18 09:00:10 +0600

    Update FeaturedProduct.jsx

--- src/components/FeaturedProduct.jsx
+++ src/components/FeaturedProduct.jsx
@@ -5,7 +5,16 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from 'react-slick';
 
-
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "red" }}
+      onClick={onClick}
+    />
+  );
+}
 
 function SamplePrevArrow(props) {
   const { className, style, onClick } = props;
@@ -18,27 +27,22 @@ function SamplePrevArrow(props) {
   );
 }
 
-function CustomArrows() {
-  const settings = {
-    dots: true,
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    nextArrow: <SampleNextArrow />,
-    prevArrow: <SamplePrevArrow />
-  };
-}
+
 
 const FeaturedProduct = () => {
 
-    var settings = {
-    dots: true,
+  var settings = {
+    dots: false,
     infinite: true,
-    speed: 50,
+    speed: 500,
     slidesToShow: 5,
-    slidesToScroll: 1
-    
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />
   };
+
 
     return (
         <>
