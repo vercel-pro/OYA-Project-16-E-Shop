6fcd0bd832815f3c8946b8be22f45b6570b92e2e
Vercel <vercel@devsramen.com>
2025-05-27 17:08:34 +0600

    Update Spring Sale at 27-05-2025_1708

--- src/components/SpringSale.jsx
+++ src/components/SpringSale.jsx
@@ -1,82 +1,105 @@
-import React, { useEffect, useState } from 'react';
-import Container from './commonLayouts/Container';
-import Slider from 'react-slick';
-import ProductLayout from './commonLayouts/ProductLayout';
-import { MdArrowBackIosNew, MdArrowForwardIos } from 'react-icons/md';
-import Button from './Button';
+import React, { useEffect, useState } from "react";
+import Container from "./commonLayouts/Container";
+import Slider from "react-slick";
+import ProductLayout from "./commonLayouts/ProductLayout";
+import { MdArrowBackIosNew, MdArrowForwardIos } from "react-icons/md";
+import Button from "./Button";
 
 const springSaleData = [
-    {
-        image:"",
-        percentTag:true,
-        categories:"TELEVISION",
-        title:"LP78245 Smart TV OLED 43 Inch 4K HD Dynamic Color Enhancer USB Movie ...",
-        rating: 1,
-        totalRating:50,
-        price:310,
-        bg:"white",
-        border: true,
-        stock:true,
-        stockAmount: 20
-    },
-    {
-        image:"",
-        percentTag:true,
-        categories:"CAMERA",
-        title:"VBI Mini 2 Fly More Combo Drone Sjrc F22S 4K Pro Ptz 5 Km Version Alpha ...",
-        rating: 2,
-        totalRating:100,
-        price:320,
-        bg:"white",
-        border: false,
-        stock:true,
-        stockAmount: 50
-    },
-    {
-        image:"",
-        percentTag:true,
-        categories:"TELEVISION",
-        title:"LP78245 Smart TV OLED 43 Inch 4K HD Dynamic Color Enhancer USB Movie ...",
-        rating: 3,
-        totalRating:200,
-        price:330,
-        bg:"white",
-        border: true,
-        stock:true,
-        stockAmount: 20
-    },
-    {
-        image:"",
-        percentTag:true,
-        categories:"TELEVISION",
-        title:"LP78245 Smart TV OLED 43 Inch 4K HD Dynamic Color Enhancer USB Movie ...",
-        rating: 4,
-        totalRating:250,
-        price:340,
-        bg:"white",
-        border: true,
-        stock:true,
-        stockAmount: 100
-    },
-    {
-        image:"",
-        percentTag:true,
-        categories:"TELEVISION",
-        title:"LP78245 Smart TV OLED 43 Inch 4K HD Dynamic Color Enhancer USB Movie ...",
-        rating: 5,
-        totalRating:300,
-        price:350,
-        bg:"white",
-        border: true,
-        stock:true,
-        stockAmount: 20
-    },
-]
+  {
+    image: "images/spring_Placeholder.svg",
+    percentTag: true,
+    categories: "TELEVISION",
+    title:
+      "LP78245 Smart TV OLED 43 Inch 4K HD Dynamic Color Enhancer USB  ...",
+    rating: 1,
+    totalRating: 50,
+    price: 310,
+    bg: "white",
+    border: true,
+    stock: true,
+    stockAmount: 20,
+  },
+  {
+    image: "images/spring_Placeholder.svg",
+    percentTag: true,
+    categories: "CAMERA",
+    title:
+      "VBI Mini 2 Fly More Combo Drone Sjrc F22S 4K Pro Ptz 5 Km Version  ...",
+    rating: 2,
+    totalRating: 100,
+    price: 320,
+    bg: "white",
+    border: false,
+    stock: true,
+    stockAmount: 50,
+  },
+  {
+    image: "images/spring_Placeholder.svg",
+    percentTag: true,
+    categories: "TELEVISION",
+    title:
+      "LP78245 Smart TV OLED 43 Inch 4K HD Dynamic Color Enhancer USB  ...",
+    rating: 3,
+    totalRating: 200,
+    price: 330,
+    bg: "white",
+    border: true,
+    stock: true,
+    stockAmount: 20,
+  },
+  {
+    image: "images/spring_Placeholder.svg",
+    percentTag: true,
+    categories: "TELEVISION",
+    title:
+      "LP78245 Smart TV OLED 43 Inch 4K HD Dynamic Color Enhancer USB  ...",
+    rating: 4,
+    totalRating: 250,
+    price: 340,
+    bg: "white",
+    border: true,
+    stock: true,
+    stockAmount: 100,
+  },
+  {
+    image: "images/spring_Placeholder.svg",
+    percentTag: true,
+    categories: "TELEVISION",
+    title:
+      "LP78245 Smart TV OLED 43 Inch 4K HD Dynamic Color Enhancer USB  ...",
+    rating: 5,
+    totalRating: 300,
+    price: 350,
+    bg: "white",
+    border: true,
+    stock: true,
+    stockAmount: 20,
+  },
+];
 
 function SamplePrevArrow(props) {
   const { className, style, onClick } = props;
   return (
-    <div className={className} style={{ ...style, color:"#303030", fontSize:"20px", border:"1px solid #303030", borderRadius:"50%", width: "40px", height:"40px", display: "flex", justifyContent:"center", alignItems:"center", position:"absolute", left:"calc(50% - 803px)", zIndex:"99"}} onClick={onClick}>
+    <div
+      className={className}
+      style={{
+        ...style,
+        color: "#303030",
+        fontSize: "20px",
+        border: "1px solid #303030",
+        borderRadius: "50%",
+        width: "40px",
+        height: "40px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        position: "absolute",
+        left: "calc(50% - 803px)",
+        zIndex: "99",
+      }}
+      onClick={onClick}
+    >
       <MdArrowBackIosNew />
     </div>
   );
@@ -85,43 +108,64 @@ function SamplePrevArrow(props) {
 function SampleNextArrow(props) {
   const { className, style, onClick } = props;
   return (
-    <div className={className} style={{ ...style, background:"white", color:"#303030", fontSize:"20px", border:"1px solid #303030", borderRadius:"50%", width: "40px", height:"40px", display: "flex", justifyContent:"center", alignItems:"center", position:"absolute", right:"0", translate: "10px", zIndex:"99"}} onClick={onClick}>
+    <div
+      className={className}
+      style={{
+        ...style,
+        background: "white",
+        color: "#303030",
+        fontSize: "20px",
+        border: "1px solid #303030",
+        borderRadius: "50%",
+        width: "40px",
+        height: "40px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        position: "absolute",
+        right: "0",
+        translate: "10px",
+        zIndex: "99",
+      }}
+      onClick={onClick}
+    >
       <MdArrowForwardIos />
     </div>
   );
 }
 
 const SpringSale = () => {
+  const [timerLeft, setTimerLeft] = useState(calculateTimeLeft());
 
-    const [timerLeft,setTimerLeft] = useState(calculateTimeLeft())
+  function calculateTimeLeft() {
+    const saleEndDate = new Date("May 31, 2025 10:00 AM +06").getTime();
+    const now = new Date().getTime();
+    const difference = saleEndDate - now;
 
-    function calculateTimeLeft (){
-        const saleEndDate = new Date('May 31, 2025 10:00 AM +06').getTime()
-        const now = new Date().getTime()
-        const difference = saleEndDate - now;
-
-        if( difference < 0 ){
-            return {days: 0, hours: 0, minutes: 0, seconds: 0 };
-        }
-        return {
-            days : Math.floor(difference / (1000 * 60 * 60 * 24)),
-            hours : Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-            minutes : Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60)),
-            seconds : Math.floor((difference % (1000 * 60) / 1000)),
-        }
+    if (difference < 0) {
+      return { days: 0, hours: 0, minutes: 0, seconds: 0 };
     }
+    return {
+      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+      hours: Math.floor(
+        (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      ),
+      minutes: Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60)),
+      seconds: Math.floor((difference % (1000 * 60)) / 1000),
+    };
+  }
 
-    useEffect(()=>{
-        const timer = setInterval(()=>{
-            setTimerLeft(calculateTimeLeft());
-        },1000)
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimerLeft(calculateTimeLeft());
+    }, 1000);
 
-        return () => clearInterval(timer);
-    },[])
+    return () => clearInterval(timer);
+  }, []);
 
-     const formatTime = (value) => String(value).padStart(2, '0');
+  const formatTime = (value) => String(value).padStart(2, "0");
 
-    var settings = {
+  var settings = {
     dots: false,
     infinite: true,
     speed: 500,
@@ -129,42 +173,100 @@ const SpringSale = () => {
     slidesToScroll: 1,
     autoplay: false,
     autoplaySpeed: 2000,
-    nextArrow: <SampleNextArrow />
+    nextArrow: <SampleNextArrow />,
   };
 
-    return (
-        <>
-            <div className={`bg-[#F4F4F4] mt-20 py-16`}>
-                <Container>
-                    <div className="flex justify-start items-center gap-x-[127px] relative">
-                        <div className={`w-[402px] flex flex-col justify-center gap-10 items-center`}>
-                            <h2 className={`font-["Poppins"] font-bold text-[56px] text-[#303030]`}>Spring Sale</h2>
-                            <div className={`flex justify-center items-start gap-x-3 font-["Poppins"] font-semibold text-[36px] text-[#FF624C]`}>
-                                <p className='flex flex-col items-center'>{formatTime(timerLeft.days)} <span className={`font-["Montserrat"] font-normal text-base text-[#303030]`}>Days</span></p>:
-                                <p className='flex flex-col items-center'>{formatTime(timerLeft.hours)} <span className={`font-["Montserrat"] font-normal text-base text-[#303030]`}>Hours</span></p>:
-                                <p className='flex flex-col items-center'>{formatTime(timerLeft.minutes)} <span className={`font-["Montserrat"] font-normal text-base text-[#303030]`}>Minutes</span></p>:
-                                <p className='flex flex-col items-center'>{formatTime(timerLeft.seconds)} <span className={`font-["Montserrat"] font-normal text-base text-[#303030]`}>Seconds</span></p>
-                            </div>
-                            <div className='w-full pl-10'>
-                                <Button text={"Shop Now"}/>
-                            </div>
-                        </div>
-                        <div className={`w-[950px]`}>
-                            <Slider {...settings} className='flex justify-center gap-1 my-7'>
-                                {
-                                    springSaleData.map((item,index)=>(
-                                        <ProductLayout key={index} width='w-[463px]' percentTag={item.percentTag} rounded={true} categories={item.categories} title={item.title} rating={item.rating} totalRating={item.totalRating} price={item.price} bg={item.bg} border={item.border} stock={item.stock} stockAmount={item.stockAmount}/>
-                                        
-                                    ))
-                                }
-                            </Slider>
-                        </div>
-                        <img src="images/SpringDots.svg" alt="SpringDots" className={`absolute left-10 bottom-0`}/>
-                    </div>
-                </Container>
+  return (
+    <>
+      <div className={`bg-[#F4F4F4] mt-20 py-16`}>
+        <Container>
+          <div className="flex justify-start items-center gap-x-[127px] relative">
+            <div
+              className={`w-[402px] flex flex-col justify-center gap-10 items-center`}
+            >
+              <h2
+                className={`font-["Poppins"] font-bold text-[56px] text-[#303030]`}
+              >
+                Spring Sale
+              </h2>
+              <div
+                className={`flex justify-center items-start gap-x-3 font-["Poppins"] font-semibold text-[36px] text-[#FF624C]`}
+              >
+                <p className="flex flex-col items-center">
+                  {formatTime(timerLeft.days)}{" "}
+                  <span
+                    className={`font-["Montserrat"] font-normal text-base text-[#303030]`}
+                  >
+                    Days
+                  </span>
+                </p>
+                :
+                <p className="flex flex-col items-center">
+                  {formatTime(timerLeft.hours)}{" "}
+                  <span
+                    className={`font-["Montserrat"] font-normal text-base text-[#303030]`}
+                  >
+                    Hours
+                  </span>
+                </p>
+                :
+                <p className="flex flex-col items-center">
+                  {formatTime(timerLeft.minutes)}{" "}
+                  <span
+                    className={`font-["Montserrat"] font-normal text-base text-[#303030]`}
+                  >
+                    Minutes
+                  </span>
+                </p>
+                :
+                <p className="flex flex-col items-center">
+                  {formatTime(timerLeft.seconds)}{" "}
+                  <span
+                    className={`font-["Montserrat"] font-normal text-base text-[#303030]`}
+                  >
+                    Seconds
+                  </span>
+                </p>
+              </div>
+              <div className="w-full pl-10">
+                <Button text={"Shop Now"} />
+              </div>
             </div>
-        </>
-    );
+            <div className={`w-[950px]`}>
+              <Slider {...settings} className="flex justify-center gap-1 my-7">
+                {springSaleData.map((item, index) => (
+                  <ProductLayout
+                    key={index}
+                    width="w-[463px]"
+                    hoverBorderColor="border-transparent"
+                    padding="p-10"
+                    bg="bg-[#eaeaea]"
+                    imageSrc={item.image}
+                    imgWith="w-[383px]"
+                    percentTag={item.percentTag}
+                    rounded={true}
+                    categories={item.categories}
+                    title={item.title}
+                    rating={item.rating}
+                    totalRating={item.totalRating}
+                    price={item.price}
+                    border={item.border}
+                    stock={item.stock}
+                    stockAmount={item.stockAmount}
+                  />
+                ))}
+              </Slider>
+            </div>
+            <img
+              src="images/SpringDots.svg"
+              alt="SpringDots"
+              className={`absolute left-10 bottom-0`}
+            />
+          </div>
+        </Container>
+      </div>
+    </>
+  );
 };
 
 export default SpringSale;
--- src/components/commonLayouts/ProductLayout.jsx
+++ src/components/commonLayouts/ProductLayout.jsx
@@ -1,64 +1,103 @@
-import React, { useState } from 'react';
-import { FaRegHeart, FaStar } from 'react-icons/fa';
-import { IoCartOutline, IoShareSocialOutline } from 'react-icons/io5';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { FaRegHeart, FaStar } from "react-icons/fa";
+import { IoCartOutline, IoShareSocialOutline } from "react-icons/io5";
+import { Link } from "react-router-dom";
 
-const ProductLayout = ({ width="w-[284px]", percentTag=false, rounded=false, categories="Category Not available",title="Title Not available",rating,totalRating,price,bg, border,stock=false,stockAmount=0 }) => {
+const ProductLayout = ({
+  width = "w-[284px]",
+  padding = "p-6",
+  hoverBorderColor = "border-[#C3C3C3]",
+  imageSrc = "images/productImage-1.png",
+  imgWith,
+  percentTag = false,
+  rounded = false,
+  categories = "Category Not available",
+  title = "Title Not available",
+  rating,
+  totalRating,
+  price,
+  bg = "bg-white",
+  border,
+  stock = false,
+  stockAmount = 0,
+}) => {
+  let [ratingValue, setRatingValue] = useState(new Array(rating).fill(rating));
 
-    let [ratingValue, setRatingValue] = useState(new Array(rating).fill(rating))
-    
-    return (
-        <div style={{background:bg}} className={`${width} border-2 border-transparent p-6 overflow-hidden rounded-lg transition-all duration-300 group hover:border-[#C3C3C3]`}>
-            <div className='relative'>
-                
-                <img src="images/productImage-1.png" alt="productImages" className='w-full' />
-                {
-                    percentTag && 
-                    <>
-                    {
-                        rounded ?
-                        <span className='bg-[#FF624c] w-[100px] h-[100px] text-base font-["Montserrat"] font-bold text-white rounded-full text-center absolute -top-2 -right-2 flex justify-center items-center'>50%</span>
-                        :
-                        <span className='bg-[#FF624c] py-[7px] px-5 text-base font-["Montserrat"] font-bold text-white rounded-md text-center absolute -top-2 -right-2'>50%</span>
-                    }
-                    </>
-                }
-                <div className='flex justify-center items-center gap-x-4 absolute left-1/2 -translate-x-1/2 bottom-1 scale-0 transition-all duration-300 group-hover:scale-100'>
-                    <Link to={"/"} className="w-[50px] h-[50px] transition-all duration-300 border border-[#FF624C] bg-white text-[#FF624C] hover:bg-[#FF624C] rounded-full hover:text-white text-2xl flex justify-center items-center">
-                        <IoCartOutline />
-                    </Link>
-                    <Link to={"/"} className="w-[50px] h-[50px] transition-all duration-300 border border-[#FF624C] bg-white text-[#FF624C] hover:bg-[#FF624C] rounded-full hover:text-white text-2xl flex justify-center items-center">
-                        <FaRegHeart />
-                    </Link>
-                    <Link to={"/"} className="w-[50px] h-[50px] transition-all duration-300 border border-[#FF624C] bg-white text-[#FF624C] hover:bg-[#FF624C] rounded-full hover:text-white text-2xl flex justify-center items-center">
-                        <IoShareSocialOutline />
-                    </Link>
-                </div>
-                
-            </div>
-            <div>
-                <p className='font-["Montserrat"] font-normal text-sm leading-5 tracking-[5px] uppercase mt-11 mb-4'>{categories}</p>
-                <h3 className='font-["Poppins"] font-semibold text-xl leading-[30px] transition-all duration-300 group-hover:text-[#FF624C] group-hover:underline'>{title}</h3>
-                <div className="flex items-center gap-2 text-[#FED550] mt-1">
-                    {
-                        ratingValue.map((item, index)=>(
-                            <FaStar key={index}/>
-                        ))
-                    }
-                    <span className='font-["Montserrat"] font-normal text-base text-[#303030]'> ( {totalRating} )</span>
-                </div>
-                <p className='font-["Poppins"] font-semibold text-2xl'>${price}</p>
-                {
-                    stock && 
-                    <div className="w-full h-[30px] bg-[#dddddd] rounded-3xl mt-8 relative">
-                        <span className='absolute top-1/2 left-1/2 -translate-1/2 text-white font-["Montserrat"] text-sm font-bold uppercase'>{stockAmount} AVAILABLE</span>
-                        <div className="w-6/10 h-[30px] bg-[#333333] rounded-3xl mt-8"></div>
-                    </div>
-
-                }
-            </div>
+  return (
+    <div
+      className={`${width} ${bg} border-2 border-transparent ${padding} overflow-hidden rounded-lg transition-all duration-300 group hover:bg-[#fff] hover:${hoverBorderColor}`}
+    >
+      <div className="relative">
+        <div className={`${imgWith} mx-auto`}>
+          <img
+            // src="images/productImage-1.png"
+            src={imageSrc}
+            alt="productImages"
+            className="w-full"
+          />
         </div>
-    );
+        {percentTag && (
+          <>
+            {rounded ? (
+              <span className='bg-[#FF624c] w-[100px] h-[100px] text-base font-["Montserrat"] font-bold text-white rounded-full text-center absolute -top-2 -right-2 flex justify-center items-center'>
+                50%
+              </span>
+            ) : (
+              <span className='bg-[#FF624c] py-[7px] px-5 text-base font-["Montserrat"] font-bold text-white rounded-md text-center absolute -top-2 -right-2'>
+                50%
+              </span>
+            )}
+          </>
+        )}
+        <div className="flex justify-center items-center gap-x-4 absolute left-1/2 -translate-x-1/2 bottom-1 scale-0 transition-all duration-300 group-hover:scale-100">
+          <Link
+            to={"/"}
+            className="w-[50px] h-[50px] transition-all duration-300 border border-[#FF624C] bg-white text-[#FF624C] hover:bg-[#FF624C] rounded-full hover:text-white text-2xl flex justify-center items-center"
+          >
+            <IoCartOutline />
+          </Link>
+          <Link
+            to={"/"}
+            className="w-[50px] h-[50px] transition-all duration-300 border border-[#FF624C] bg-white text-[#FF624C] hover:bg-[#FF624C] rounded-full hover:text-white text-2xl flex justify-center items-center"
+          >
+            <FaRegHeart />
+          </Link>
+          <Link
+            to={"/"}
+            className="w-[50px] h-[50px] transition-all duration-300 border border-[#FF624C] bg-white text-[#FF624C] hover:bg-[#FF624C] rounded-full hover:text-white text-2xl flex justify-center items-center"
+          >
+            <IoShareSocialOutline />
+          </Link>
+        </div>
+      </div>
+      <div>
+        <p className='font-["Montserrat"] font-normal text-sm leading-5 tracking-[5px] uppercase mt-11 mb-4'>
+          {categories}
+        </p>
+        <h3 className='font-["Poppins"] font-semibold text-xl leading-[30px] transition-all duration-300 group-hover:text-[#FF624C] group-hover:underline'>
+          {title}
+        </h3>
+        <div className="flex items-center gap-2 text-[#FED550] mt-1">
+          {ratingValue.map((item, index) => (
+            <FaStar key={index} />
+          ))}
+          <span className='font-["Montserrat"] font-normal text-base text-[#303030]'>
+            {" "}
+            ( {totalRating} )
+          </span>
+        </div>
+        <p className='font-["Poppins"] font-semibold text-2xl'>${price}</p>
+        {stock && (
+          <div className="w-full h-[30px] bg-[#dddddd] rounded-3xl mt-8 relative">
+            <span className='absolute top-1/2 left-1/2 -translate-1/2 text-white font-["Montserrat"] text-sm font-bold uppercase'>
+              {stockAmount} AVAILABLE
+            </span>
+            <div className="w-6/10 h-[30px] bg-[#333333] rounded-3xl mt-8"></div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default ProductLayout;
