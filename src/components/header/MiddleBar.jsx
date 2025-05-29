57bea8b2117c2337deeba82b517cf0657e81d427
Vercel <vercel@devsramen.com>
2025-05-29 16:15:50 +0600

    Update MiddleBar.jsx

--- src/components/header/MiddleBar.jsx
+++ src/components/header/MiddleBar.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Container from "../commonLayouts/Container";
-import { IoSearchSharp } from "react-icons/io5";
 import CartIcon from "../../assets/icons/CartIcon";
 import UserIcon from "../../assets/icons/UserIcon";
 import { Link } from "react-router-dom";
@@ -10,7 +9,7 @@ const MiddleBar = () => {
   return (
     <>
       <Container>
-        <div className="flex justify-between items-center py-8">
+        <div className="flex justify-between items-center py-6">
           <div>
             <Link to={"/"}>
               <img
@@ -33,22 +32,22 @@ const MiddleBar = () => {
             </div>
             <div className="w-[150px] relative pl-[50px] mr-16">
               <CartIcon width={35} height={35} color="#303030" />
-              <div className="absolute top-1/2 -translate-y-1/2 left-24">
-                <p className='font-["Montserrat"] font-normal text-base leading-6'>
+              <div className=" text-black absolute top-1/2 -translate-y-1/2 left-24">
+                <p className='font-["Montserrat"] font-normal text-base capitalize leading-6'>
                   cart
                 </p>
-                <span className='font-["Montserrat"] font-bold text-base leading-5'>
+                <span className='font-["Montserrat"] font-bold text-base leading-6'>
                   $150,000
                 </span>
               </div>
             </div>
-            <div className='w-[170px] relative pl-[50px] after:content-[""] after:w-[1px] after:h-8 after:bg-[#303030] after:absolute after:left-0 after:top-1/2 after:-translate-y-1/2'>
+            <div className='w-[170px] text-black  relative pl-[50px] after:content-[""] after:w-[1px] after:h-8 after:bg-[#303030] after:absolute after:left-0 after:top-1/2 after:-translate-y-1/2'>
               <UserIcon width={28} height={28} color="#303030" />
               <div className="absolute top-1/2 -translate-y-1/2 left-24">
-                <p className='font-["Montserrat"] font-normal text-base leading-6'>
+                <p className='font-["Montserrat"] font-normal text-base capitalize leading-6'>
                   User
                 </p>
-                <span className='font-["Montserrat"] font-bold text-base leading-5'>
+                <span className='font-["Montserrat"] font-bold text-base leading-6'>
                   Account
                 </span>
               </div>
